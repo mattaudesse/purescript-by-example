4f53afe31c51d2f68b951d764ff7816443fd1020
Matt Audesse <matt@mattaudesse.com>
2018-01-19 13:13:14 -0500

    Replace bower w/ psc, support PS 0.11.7, +tidy chapter 3

--- chapter3/src/Main.purs
+++ chapter3/src/Main.purs
@@ -1,15 +1,15 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
-import Data.List
-import Data.Maybe
-import Control.Plus     (empty)
+import Control.Monad.Eff         (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Data.List                 (List(..), filter, head, intercalate)
+import Data.Maybe                (Maybe)
+import Control.Plus              (empty)
 
-type Address = 
+type Address =
   { street :: String
-  , city   :: String 
+  , city   :: String
   , state  :: String
   }
 
@@ -22,22 +22,26 @@ type Entry =
 type AddressBook = List Entry
 
 showAddress :: Address -> String
-showAddress addr = addr.street ++ ", " ++ addr.city ++ ", " ++ addr.state
+showAddress a = intercalate ", " [ a.street, a.city, a.state ]
 
 showEntry :: Entry -> String
-showEntry entry = entry.lastName ++ ", " ++ entry.firstName ++ ": " ++ showAddress entry.address
+showEntry e =
+       e.lastName
+    <> ", "
+    <> e.firstName
+    <> ": "
+    <> showAddress e.address
 
 emptyBook :: AddressBook
-emptyBook = empty
+emptyBook =  empty
 
 insertEntry :: Entry -> AddressBook -> AddressBook
-insertEntry = Cons
- 
+insertEntry =  Cons
+
 findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName = head <<< filter filterEntry 
-  where
-  filterEntry :: Entry -> Boolean
-  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+findEntry firstName lastName = head <<< filter f
+  where f e =  e.firstName == firstName
+            && e.lastName  == lastName
 
 
 --- Chapter 3, exercise 2:
@@ -45,33 +49,30 @@ findEntry firstName lastName = head <<< filter filterEntry
 --- by reusing the existing code in findEntry. Test your function in PSCi.
 
 findEntryByAddress :: Address -> AddressBook -> Maybe Entry
-findEntryByAddress address = head <<< filter filterEntry
-    where
-    filterEntry :: Entry -> Boolean
-    filterEntry entry =  entry.address.street   == address.street
-                      && entry.address.city     == address.city
-                      && entry.address.state    == address.state
+findEntryByAddress address = head <<< filter f
+    where f e =  e.address.street == address.street
+              && e.address.city   == address.city
+              && e.address.state  == address.state
 
 
 demoExerciseTwo :: forall e. Eff (console :: CONSOLE | e) Unit
 demoExerciseTwo = do
-    let a1 = { street: "123 Main St.",  city: "Rockland",   state: "Maine"  }
-        a2 = { street: "456 Farm Road", city: "Orono",      state: "Maine"  }
-        a3 = { street: "789 No Blvd.",  city: "Portland",   state: "Maine"  }
-        e1 = { firstName: "Joe",    lastName: "Schmoe",     address: a1     }
-        e2 = { firstName: "John",   lastName: "Doe",        address: a2     }
+    let a1 = { street:    "123 Main St.",  city:     "Rockland", state:   "Maine" }
+        a2 = { street:    "456 Farm Road", city:     "Orono",    state:   "Maine" }
+        a3 = { street:    "789 No Blvd.",  city:     "Portland", state:   "Maine" }
+        e1 = { firstName: "Joe",           lastName: "Schmoe",   address: a1      }
+        e2 = { firstName: "John",          lastName: "Doe",      address: a2      }
         book = insertEntry e2 $ insertEntry e1 emptyBook
 
     log "(\"a1\" and \"a2\" are addresses in \"book\"; \"a3\" is not)\n"
 
     log "> findEntryByAddress a1 book ="
-    print $ showEntry <$> findEntryByAddress a1 book
+    logShow $ showEntry <$> findEntryByAddress a1 book
 
     log "\n"
     log "> findEntryByAddress a3 book ="
-    print $ showEntry <$> findEntryByAddress a3 book
+    logShow $ showEntry <$> findEntryByAddress a3 book
 
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-    demoExerciseTwo
+main =  demoExerciseTwo
